1220c5e90c0ace1c981b0c74ffc141881bfe5d52
Praneeth Patakota <praneeth001@gmail.com>
2020-06-25 21:31:40 +0530

    Removed Commander and Added main and submenus using Inquirer module itself. so overall no need of python menus anymore

--- index.js
+++ index.js
@@ -1,71 +1,241 @@
 #!/usr/bin/env node
 'use strict';
-const path = require('path');
-const program = require('commander');
 
+const inquirer = require('inquirer');
 const list = require('./lib/list');
 
-const pkg = require(path.join(__dirname, 'package.json'));
+const questions_categories = [
+  {
+    type: 'list',
+    name: 'category',
+    message: 'Select a category',
+    choices: [
+      {
+        name: 'Editorials',
+        value: 'eds'
+      },
+      {
+        name: 'Magazines',
+        value: 'magz'
+      },
+      {
+        name: 'Sports',
+        value: 'sports'
+      },
+      {
+        name: 'Tech',
+        value: 'tech'
+      },
+      {
+        name: 'Science',
+        value: 'science'
+      },
+    ]
+  }
+];
 
-program.version(pkg.version).description(pkg.description);
+const questions_editorials = [
+  {
+    type: 'list',
+    name: 'source',
+    message: 'Editorials/Columns: Select a source ',
+    choices: [
+      {
+        name: 'The Hindu',
+        value: 'hindu'
+      },
+      {
+        name: 'The Hindu lead Opinion',
+        value: 'hindulead'
+      },
+      {
+        name: 'Economic Times',
+        value: 'et'
+      },
+      {
+        name: 'Indian Express',
+        value: 'ie'
+      },
+      {
+        name: 'Indian Express Columns',
+        value: 'iecolumns'
+      },
+      {
+        name: 'Hindu Business Line',
+        value: 'hbl'
+      },
+      {
+        name: 'Guardian',
+        value: 'guardian'
+      },
+      {
+        name: 'Live Mint',
+        value: 'livemint'
+      }
+    ]
+  }
+];
 
-program
-  .command('eds')
-  .arguments('<source>')
-  .description(
-    'List Editorials from TheHindu, ET, Guardian, HinduBusinessLine, IndianExpress'
-  )
-  .action(function(source, options) {
-    list('eds', {
-      value: source
+const questions_magazines = [
+  { 
+    type: 'list',
+    name: 'source',
+    message: 'Magazines: Select a source ',
+    choices: [
+      {
+        name: 'The Economist',
+        value: 'economist'
+      },
+      {
+        name: 'Project Syndicate',
+        value: 'projsyn'
+      },
+      {
+        name: 'Harvard Business Review',
+        value: 'hbr'
+      },
+      {
+        name: 'The Atlantic',
+        value: 'atlantic'
+      },
+      {
+        name: 'Frontline',
+        value: 'frontline'
+      },
+      {
+        name: 'LongReads',
+        value: 'longreads'
+      },
+      {
+        name: 'Outlook',
+        value: 'outlook'
+      },
+      {
+        name: 'The Hindu Explains',
+        value: 'hinduexpl'
+      }
+    ]
+  }
+];
+
+const questions_tech = [
+  { 
+    type: 'list',
+    name: 'source',
+    message: 'Technology: Select a source ',
+    choices: [
+      {
+        name: 'TechCrunch',
+        value: 'techcrunch'
+      },
+      {
+        name: 'The Next Web',
+        value: 'tnw'
+      },
+      {
+        name: 'ReadWrite',
+        value: 'readwrite'
+      },
+      {
+        name: 'Business Insider',
+        value: 'bi'
+      }
+    ]
+  }
+];
+
+const questions_sports = [
+  { 
+    type: 'list',
+    name: 'source',
+    message: 'Sports: Select a source ',
+    choices: [
+      {
+        name: 'ESPN F1',
+        value: 'espnf1'
+      },
+      {
+        name: 'Auto F1',
+        value: 'autof1'
+      },
+      {
+        name: 'Premier League',
+        value: 'epl'
+      }
+    ]
+  }
+];
+
+const questions_science = [
+  { 
+    type: 'list',
+    name: 'source',
+    message: 'Science: Select a source ',
+    choices: [
+      {
+        name: 'Scientific American',
+        value: 'sciam'
+      },
+      {
+        name: 'Popular Science',
+        value: 'popsci'
+      }
+    ]
+  }
+];
+
+
+function main_menu() {
+  inquirer
+    .prompt(questions_categories)
+    .then(answers => { 
+      if(answers.category == 'eds') {
+        inquirer
+          .prompt(questions_editorials)
+          .then(answers => {
+              list('eds', {
+                value: answers.source
+              })
+            }
+          )
+      } else if (answers.category == 'magz') {
+        inquirer
+          .prompt(questions_magazines)
+          .then(answers => {
+              list('magz', {
+                value: answers.source
+              })
+            }
+          )
+      } else if (answers.category == 'sports') {
+        inquirer
+          .prompt(questions_sports)
+          .then(answers => {
+              list('sports', {
+                value: answers.source
+              })
+            }
+          )
+      } else if (answers.category == 'tech') {
+        inquirer
+          .prompt(questions_tech)
+          .then(answers => {
+              list('tech', {
+                value: answers.source
+              })
+            }
+          )
+      } else if (answers.category == 'science') {
+        inquirer
+          .prompt(questions_science)
+          .then(answers => {
+              list('science', {
+                value: answers.source
+              })
+            }
+          )
+      }
     });
-  });
-
-program
-  .command('magz')
-  .arguments('<source>')
-  .description(
-    'List Editorials from HarvardBusinessReview, ProjectSyndicate, Economist, NewYorker, TheAtlantic'
-  )
-  .action(function(source, options) {
-    list('magz', {
-      value: source
-    });
-  });
-
-program
-  .command('sports')
-  .arguments('<sport>')
-  .description('List trending Sports Stories from F1, EPL world')
-  .action(function(sport, options) {
-    list('sports', {
-      value: sport
-    });
-  });
-
-program
-  .command('science')
-  .arguments('<source>')
-  .description('List articles from PopSci and SciAmerican')
-  .action(function(source, options) {
-    list('science', {
-      value: source
-    });
-  });
-
-program
-  .command('tech')
-  .arguments('<source>')
-  .description('List articles from PopSci and SciAmerican')
-  .action(function(source, options) {
-    list('tech', {
-      value: source
-    });
-  });
-
-program.parse(process.argv);
-
-if (!process.argv.slice(2).length) {
-  // Show help by default
-  program.outputHelp();
 }
+
+main_menu();
